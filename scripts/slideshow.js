326b4bf340ad6235463247e7df0f7f0674f57b39
Angela Busato <busato.angela@gmail.com>
2021-03-24 12:00:08 +0100

    [add] get data from a mock api

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,24 +1,28 @@
-const usersComments = [{
-    image: "./img/slider-profile-img.svg",
-    name: "Darren Wilson",
-    comment: "Good for scores and commentary but the team lineups have stopped working."
-}, {
-    image: "./img/slider-profile-img2.svg",
-    name: "Michael Akinuli",
-    comment: "Good for watching the goals and great for highlights instead of waiting till 10:30 before you can watch highlights on March of the day also no need of sky go"
-}, {
-    image: "./img/slider-profile-img3.svg",
-    name: "Noah Jackson",
-    comment: "I use this app every day and it's a great way to keep up to date with what's going on in football. It's a shame it won't rotate into landscape mode like its main competitor"
-}, {
-    image: "./img/slider-profile-img4.svg",
-    name: "Sarah Udoma",
-    comment: "This app has videos for other premier league teams who play and it also gives me nofications when my favourite team plays"
-}];
-
+/**
+ * Class Slideshow
+ */
 class Slideshow {
     constructor() {
         this.slideIndex = 1;
+        this.usersComments = [];
+        this.getData();
+    }
+
+    /**
+     * Gets data from API
+     */
+    async getData() {
+        try {
+            const response = await fetch('https://605a21feb11aba001745da26.mockapi.io/api/v1/comments');
+
+            this.usersComments = await response.json();
+
+            this.displayData();
+            this.displayDot();
+
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     /**
@@ -26,52 +30,58 @@ class Slideshow {
      */
     displayData() {
 
-        usersComments.forEach(item => {
-            var slide = document.createElement("div");
-            slide.setAttribute("class", "slideshow__slide");
-            var slideUser = document.createElement("div");
-            slideUser.setAttribute("class", "slide__user flex flex-row justify-between align-center");
-            slide.appendChild(slideUser);
+        if (this.usersComments) {
 
-            var userImg = document.createElement("span");
-            userImg.setAttribute("id", "user-img");
-            userImg.innerHTML = '<img src="' + item.image + '" alt="Profile image"/>';
-            slideUser.appendChild(userImg);
+            this.usersComments.forEach(item => {
+                var slide = document.createElement("div");
+                slide.setAttribute("class", "slideshow__slide");
+                var slideUser = document.createElement("div");
+                slideUser.setAttribute("class", "slide__user flex flex-row justify-between align-center");
+                slide.appendChild(slideUser);
 
-            var userName = document.createElement("span");
-            userName.setAttribute("id", "user-name");
-            userName.innerHTML = item.name;
-            slideUser.appendChild(userName);
+                var userImg = document.createElement("span");
+                userImg.setAttribute("id", "user-img");
+                userImg.innerHTML = '<img src="' + item.image + '" alt="Profile image"/>';
+                slideUser.appendChild(userImg);
 
-            var twitter = document.createElement("img");
-            twitter.setAttribute("src", "img/twitter.svg");
-            twitter.setAttribute("alt", "Twitter");
-            slideUser.appendChild(twitter);
+                var userName = document.createElement("span");
+                userName.setAttribute("id", "user-name");
+                userName.innerHTML = item.name;
+                slideUser.appendChild(userName);
 
-            var slideUserComment = document.createElement("div");
-            slideUserComment.setAttribute("class", "user-comment");
-            slideUserComment.setAttribute("id", "user-commnet");
-            var textComment = document.createTextNode(item.comment);
-            slideUserComment.appendChild(textComment);
-            slide.appendChild(slideUserComment);
+                var twitter = document.createElement("img");
+                twitter.setAttribute("src", "img/twitter.svg");
+                twitter.setAttribute("alt", "Twitter");
+                slideUser.appendChild(twitter);
 
-            document.getElementById("slideshow-wrapper").appendChild(slide);
-        });
+                var slideUserComment = document.createElement("div");
+                slideUserComment.setAttribute("class", "user-comment");
+                slideUserComment.setAttribute("id", "user-commnet");
+                var textComment = document.createTextNode(item.comment);
+                slideUserComment.appendChild(textComment);
+                slide.appendChild(slideUserComment);
+
+                document.getElementById("slideshow-wrapper").appendChild(slide);
+            });
+        }
     }
 
     /**
      * Creates and displays the dots for the slideshow
      */
     displayDot() {
-        usersComments.forEach((item, index) => {
-            var dot = document.createElement("button");
-            dot.setAttribute("name", "dot");
-            dot.setAttribute("class", "dot");
-            dot.setAttribute("aria-label", "Dot Button");
-            dot.setAttribute("id", index + 1);
+        if (this.usersComments) {
+            this.usersComments.forEach((item, index) => {
+                var dot = document.createElement("button");
+                dot.setAttribute("name", "dot");
+                dot.setAttribute("class", "dot");
+                dot.setAttribute("aria-label", "Dot Button");
+                dot.setAttribute("id", index + 1);
 
-            document.getElementById("dot-container").appendChild(dot);
-        });
+                document.getElementById("dot-container").appendChild(dot);
+            });
+        }
+
         this.showSlides(this.slideIndex);
     }
 
@@ -88,7 +98,6 @@ class Slideshow {
      * @param {Number} n  
      */
     showSlides(n) {
-        console.log(n);
         var slides = document.getElementsByClassName("slideshow__slide");
         var dots = document.getElementsByClassName("dot");
 
@@ -114,9 +123,6 @@ class Slideshow {
      * @param {Number} n 
      */
     plusSlides(n) {
-        console.log("plus");
-        console.log(this.slideIndex);
-        console.log(n);
         this.showSlides(this.slideIndex += n);
     }
 }
@@ -126,8 +132,6 @@ const nextButton = document.querySelector('.next');
 const prevButton = document.querySelector('.prev');
 
 var slideshow = new Slideshow();
-slideshow.displayData();
-slideshow.displayDot();
 
 dotButton.forEach(button => {
     button.addEventListener('click', () => {
@@ -138,6 +142,7 @@ dotButton.forEach(button => {
 nextButton.addEventListener('click', () => {
     slideshow.plusSlides(1);
 });
+
 prevButton.addEventListener('click', () => {
     slideshow.plusSlides(-1);
 });
